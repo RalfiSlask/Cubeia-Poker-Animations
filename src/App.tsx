51f65abea43260aff22a0e392ed89aac1a97d6bb
RalfiSlask <nilssonnarki@gmail.com>
2024-05-27 14:09:56 +0200

    ✨ update app with relevent data

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import UserComponent from './components/UserComponent';
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <main className="flex flex-col items-center w-full pt-20 gap-20">
+        <h1 className="text-2xl">Cubeia Poker Animations</h1>
+        <section className="w-full flex justify-center">
+          <UserComponent />
+        </section>
+      </main>
+    </>
+  );
 }
 
 export default App;
