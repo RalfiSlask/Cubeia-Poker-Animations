110511f683dd044d3a98ff6f338d29bf7ee1d27e
RalfiSlask <nilssonnarki@gmail.com>
2024-05-27 14:07:14 +0200

    🏷️ add types for animation, text, buttons

---
+++ src/utils/types/types.ts
@@ -0,0 +1,26 @@
+export interface IAnimationType {
+  1: boolean;
+  2: boolean;
+  3: boolean;
+  4: boolean;
+  5: boolean;
+  6: boolean;
+  7: boolean;
+  8: boolean;
+  9: boolean;
+  10: boolean;
+}
+
+export interface ITextAnimationType {
+  id: number;
+  type: string;
+  active: boolean;
+}
+
+export interface IButtonType {
+  animationId?: keyof IAnimationType;
+  textId: number;
+  start?: number;
+  end?: number;
+  title: string;
+}
