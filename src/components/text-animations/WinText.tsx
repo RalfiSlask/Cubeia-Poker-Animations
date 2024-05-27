fef3c4fc5d5a2b0ea78841ef8aeb79eee45dfc91
RalfiSlask <nilssonnarki@gmail.com>
2024-05-27 14:23:31 +0200

    🚀 update path to gh-pages

--- src/components/text-animations/WinText.tsx
+++ src/components/text-animations/WinText.tsx
@@ -22,7 +22,7 @@ const WinText = () => {
         textShadow: '0 0 10px rgba(0, 0, 0, 0.5), 0 0 20px rgba(0, 0, 0, 0.5), 0 0 30px rgba(0, 0, 0, 0.5)',
       }}
     >
-      <source src="/win.webm" type="video/webm" />
+      <source src="/Cubeia-Poker-Animations/win.webm" type="video/webm" />
       Your browser does not support the video tag.
     </motion.video>
   );
