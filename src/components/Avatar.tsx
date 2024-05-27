7a717bfcecb7ced63649a3eda357f29f65cad765
RalfiSlask <nilssonnarki@gmail.com>
2024-05-27 14:08:58 +0200

    ✨ add avatar component

---
+++ src/components/Avatar.tsx
@@ -0,0 +1,7 @@
+import avatar from '../assets/icons/avatar.png';
+
+const Avatar = () => {
+  return <img src={avatar} alt="poker avatar" width="140" height="140" className="absolute bottom-20"></img>;
+};
+
+export default Avatar;
