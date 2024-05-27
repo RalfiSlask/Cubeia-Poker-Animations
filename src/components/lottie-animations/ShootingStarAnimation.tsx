54648200d219a3566d557d138f477bb1212ca516
RalfiSlask <nilssonnarki@gmail.com>
2024-05-27 14:07:25 +0200

    ✨ add shooting animation component

--- src/components/lottie-animations/ShootingStarAnimation.tsx
+++ src/components/lottie-animations/ShootingStarAnimation.tsx
@@ -11,8 +11,8 @@ const ShootingStarAnimation = () => {
   const { View: View1 } = useLottie(options);
 
   return (
-    <div className="ml-6 flex items-center justify-between absolute bottom-20">
-      <div className="w-[250px] h-[150px] z-10">{View1}</div>
+    <div className="ml-6 flex items-center z-30 justify-between absolute bottom-20">
+      <div className="w-[250px] h-[150px]">{View1}</div>
     </div>
   );
 };
